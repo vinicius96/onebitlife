07004045fe8d10996bb523000c871c2c15d3b74c
Vinícius Geraldo <viniciusgeraldo96@hotmail.com>
2022-11-30 22:07:03 -0300

    Inserindo animação - LifeStatus e estruturando componentes

---
+++ src/Components/Common/LifeStatus/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+import Lottie from 'lottie-react-native'
+
+export default function LifeStatus(){
+    return(
+        <View style={StyleSheet.container}>
+            <Lottie
+                source={require("../../../assets/education/education-100.json")}
+                autoPlay
+                loop
+                style={styles.educacaoAnimacao}
+            />
+            <Lottie
+                source={require("../../../assets/money/money-100.json")}
+                autoPlay
+                loop
+                style={styles.financasAnimacao}
+            />
+            <Lottie
+                source={require("../../../assets/robot/robot-100-100.json")}
+                autoPlay
+                loop
+                style={styles.roboAnimacao}
+            />
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        width: 300,
+        height: 300,
+    },
+    roboAnimacao:{
+        width: 190,
+        marginTop: 30,
+    },
+    educacaoAnimacao:{
+        width: 100,
+        marginTop: 50,
+        marginLeft: -25,
+        position: 'absolute'
+    },
+    financasAnimacao:{
+        width: 100,
+        marginTop: 50,
+        marginLeft: 75,
+        position: 'absolute'
+    }
+})
